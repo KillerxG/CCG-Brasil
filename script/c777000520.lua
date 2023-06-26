10945a285d899756bf82092ae942652bd988016e
KillerxG <54422841+KillerxG@users.noreply.github.com>
2023-06-26 09:34:58 -0300

    Fix Arknights - Battlefield

--- script/c777000520.lua
+++ script/c777000520.lua
@@ -83,6 +83,5 @@ function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ev)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Damage(p,d,REASON_EFFECT)
+	Duel.Damage(1-tp,ev*2,REASON_EFFECT)
 end
