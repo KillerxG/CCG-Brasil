2e0fcaadbda006b89566ed856260b3bc02bdc1c8
KillerxG <54422841+KillerxG@users.noreply.github.com>
2023-10-04 10:59:12 -0300

    Cheat Code Kuriboh & Weast Royal Dragon Knight
    Cheat Code Kuriboh: Artwork Fix. Found some bugs with its GY effect.
    
    Weast Royal Dragon Knight: Effect change.

--- script/c777003980.lua
+++ script/c777003980.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
 	c:RegisterEffect(e2)
-	--Increase Level
+	--(3)Increase Level
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_LVCHANGE)
@@ -29,25 +29,17 @@ function s.initial_effect(c)
 	e3:SetOperation(s.lvop)
 	c:RegisterEffect(e3)
 end
---
+--(3)Increase Level
 function s.cfilter(c,tp)
 	return c:IsLevelAbove(1) and Duel.IsExistingTarget(aux.AND(Card.IsFaceup,Card.HasLevel),tp,LOCATION_MZONE,0,1,c)
 end
-function s.spcostfilter(c)
-	return c:IsRitualMonster() and not c:IsPublic()
-end
 function s.lvcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_EXTRA,0,1,nil,tp) 
-		and Duel.IsExistingMatchingCard(s.spcostfilter,tp,LOCATION_HAND,0,1,c) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_EXTRA,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_EXTRA,0,1,1,nil,tp)
-	local f=Duel.SelectMatchingCard(tp,s.spcostfilter,tp,LOCATION_HAND,0,1,1,c)
 	Duel.SendtoGrave(g,REASON_COST)
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 	e:SetLabel(g:GetFirst():GetLevel())
-	Duel.ConfirmCards(1-tp,f)
-	Duel.ShuffleHand(tp)
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:HasLevel() end
@@ -67,5 +59,41 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(e:GetLabel())
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
+		Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+		--Check for the Special Summon of a Ritual Monster
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+		e2:SetOperation(s.regop)
+		e2:SetLabel(1)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+		--Lose 2000 LP in the End Phase
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_PHASE+PHASE_END)
+		e3:SetCountLimit(1)
+		e3:SetCondition(s.damcon)
+		e3:SetOperation(s.damop)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		e3:SetLabelObject(e2)
+		Duel.RegisterEffect(e3,tp)
 	end
+end
+function s.ritfilter(c,tp)
+	return c:IsType(TYPE_RITUAL) and c:IsSummonPlayer(tp) and c:IsFaceup()
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then return end
+	if eg and eg:IsExists(s.ritfilter,1,nil,tp) then
+		e:SetLabel(0)
+	end
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetLabel()~=0
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	local lp=Duel.GetLP(tp)
+	Duel.SetLP(tp,lp-2000)
 end
