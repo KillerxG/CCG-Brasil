cc6cab50a729f9b8d20f96c03d92f5d4691f682c
KillerxG <guiros17@hotmail.com>
2023-12-13 21:21:55 -0300

    Shinigami Grimoire
    Fixed a bug that does not allow it to activate its effects.

--- script/c777002140.lua
+++ script/c777002140.lua
@@ -7,40 +7,81 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--(1)Force Activation
+	--(1)Swap Curses
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_POSITION)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.activate)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--(2)Swap Curses
+	--(2)Tribute
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_RELEASE)
 	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1)
-	e3:SetTarget(s.thtg)
-	e3:SetOperation(s.thop)
+	e3:SetTarget(s.reltg)
+	e3:SetOperation(s.relop)
 	c:RegisterEffect(e3)
-	--(3)Tribute
+	--(3)Force Activation
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetCategory(CATEGORY_RELEASE)
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetCountLimit(1)
-	e4:SetTarget(s.reltg)
-	e4:SetOperation(s.relop)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_POSITION)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.activate)
 	c:RegisterEffect(e4)
 end
---(1)Force Activation
+--(1)Swap Curses
+function s.thfilter(c,tp,e)
+	return c:IsAbleToHand() and c:IsFacedown()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.thfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,0,LOCATION_SZONE,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,0,LOCATION_SZONE,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.tffilter(c)
+	return c:IsType(TYPE_TRAP) and c:IsSetCard(0x304b) and not c:IsForbidden()
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 then
+		local g=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK,0,tp,tp,e,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tf=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	if tf and Duel.MoveToField(tf,tp,1-tp,LOCATION_SZONE,POS_FACEUP,true) then
+		end
+	end
+end
+--(2)Tribute
+function s.filter2(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsReleasableByEffect()
+end
+function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE+LOCATION_HAND,0,1,nil) end
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+function s.relop(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE+LOCATION_HAND,0,nil)
+		if #mg>0 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+			local sg=mg:Select(tp,1,1,nil)
+			Duel.Release(sg,REASON_EFFECT)			
+		end
+end
+--(3)Force Activation
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,PLAYER_ALL,LOCATION_ONFIELD)
@@ -130,46 +171,3 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp,chk)
 		end
 	end
 end
---(2)Swap Curses
-function s.thfilter(c,tp,e)
-	return c:IsAbleToHand() and c:IsFacedown()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.thfilter(chkc) and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,0,LOCATION_SZONE,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.thfilter,tp,0,LOCATION_SZONE,1,1,e:GetHandler())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-end
-function s.tffilter(c)
-	return c:IsType(TYPE_TRAP) and c:IsSetCard(0x304b) and not c:IsForbidden()
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 then
-		local g=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK,0,tp,tp,e,tp)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local tf=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
-	if tf and Duel.MoveToField(tf,tp,1-tp,LOCATION_SZONE,POS_FACEUP,true) then
-		end
-	end
-end
---(3)Tribute
-function s.filter2(c)
-	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsReleasableByEffect()
-end
-function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE+LOCATION_HAND,0,1,nil) end
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-end
-function s.relop(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE+LOCATION_HAND,0,nil)
-		if #mg>0 then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			local sg=mg:Select(tp,1,1,nil)
-			Duel.Release(sg,REASON_EFFECT)			
-		end
-end
-
---   Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE+LOCATION_HAND,0,e:GetHandler())
