d292d16a2e4688f846ad7f9bf3130dfbfaaa3c5b
KillerxG <guiros17@hotmail.com>
2024-01-16 14:06:28 -0300

    New Card
    Phantom Gunners Tactics

---
+++ script/c777000540.lua
@@ -0,0 +1,70 @@
+--Phantom Gunners Tactics
+--Scripted by KillerxG
+local s,id=GetID()
+function s.initial_effect(c)
+	--(1)Destroy Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DECKDES)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	--(2)Set Soul Levy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id+1)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.settg)
+	e3:SetOperation(s.setop)
+	c:RegisterEffect(e3)
+end
+--(1)Destroy Deck
+function s.dkfilter(c)
+	return c:IsSetCard(0x302) and c:IsMonster()
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x302) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(s.dkfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return ct>0 and Duel.IsPlayerCanDiscardDeck(1-tp,ct) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,ct)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ct=Duel.GetMatchingGroupCount(s.dkfilter,tp,LOCATION_GRAVE,0,nil)
+	if ct>0 then
+		if Duel.DiscardDeck(1-tp,ct,REASON_EFFECT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.BreakEffect()
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+					if #sg>0 then
+						Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+					end
+		end
+	end
+end
+--(2)Set Soul Levy
+function s.filter(c)
+	return c:IsCode(87844926) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+	end
+end
