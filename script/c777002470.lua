83ccdbb7e2f0732edca5926109385b4db7ff0766
KillerxG <guiros17@hotmail.com>
2024-09-18 20:35:20 -0300

    Card Rework
    • Crimson Rose Warrior

--- script/c777002470.lua
+++ script/c777002470.lua
@@ -1,123 +1,114 @@
 --Crimson Rose Warrior
 --Scripted by KillerxG
 local s,id=GetID()
-local TYPES=TYPE_FUSION+TYPE_RITUAL+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK
 function s.initial_effect(c)
 	--Link Summon
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
 	c:EnableReviveLimit()
-	--(1)Check materials for a Link Monster
+	--(1)Register Effect: Destroy
 	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_MATERIAL_CHECK)
-	e0:SetValue(s.valcheck)
+	e0:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e0:SetCondition(s.regcon)
+	e0:SetOperation(s.regop)
 	c:RegisterEffect(e0)
-	--(2)Increase ATK
+	--(1.1)Material Count Check
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.atkcon)
-	e1:SetTarget(s.atktg)
-	e1:SetOperation(s.atkop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(s.valcheck)
 	e1:SetLabelObject(e0)
 	c:RegisterEffect(e1)
-	--(3)Destroy 1 card the opponent controls
+	--(2)Destroy replace
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(s.descost)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.reptg)
+	e2:SetOperation(s.repop)
 	c:RegisterEffect(e2)
-	--(3.1)Register Attributes used
-	aux.GlobalCheck(s,function()
-		s.attr_list={}
-		s.attr_list[0]=0
-		s.attr_list[1]=0
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_PHASE+PHASE_END)
-		ge1:SetCountLimit(1)
-		ge1:SetCondition(s.resetop)
-		Duel.RegisterEffect(ge1,0)
-	end)
+	--(3)Double damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e3:SetCondition(s.damcon)
+	e3:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
+	c:RegisterEffect(e3)
 end
---(1)Check materials for a Link Monster
+--(1.1)Material Count Check
 function s.valcheck(e,c)
 	local g=c:GetMaterial()
-	e:SetLabel(0)
-	if g:IsExists(Card.IsMonster,1,nil) then
-		e:SetLabel(1)
-	end
+	local ct=g:FilterCount(Card.IsRace,nil,RACE_WARRIOR)
+	e:GetLabelObject():SetLabel(ct)
 end
---(2)Increase ATK
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+--(1)Register Effect: Destroy
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_LINK) and e:GetLabelObject():GetLabel()==1
+	return c:IsSummonType(SUMMON_TYPE_LINK) and e:GetLabel()>0
 end
-function s.atkfilter(c,e)
-	return (c:IsLocation(LOCATION_GRAVE) or (c:IsLocation(LOCATION_REMOVED) and c:IsFaceup())) and c:IsCanBeEffectTarget(e)
+function s.chkfilter(c,label)
+	return c:GetFlagEffect(label)>0
 end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=c:GetMaterial():Filter(s.atkfilter,nil,e)
-	if chkc then return c:GetMaterial():IsContains(chkc) and s.atkfilter(chkc,e) end
-	if chk==0 then return #g>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local tg=c:GetMaterial():FilterSelect(tp,s.atkfilter,1,1,nil,e)
-	Duel.SetTargetCard(tg)
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsFaceup() and c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		--Increase ATK
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(tc:GetAttack()/2)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-	end
-end
---(3)Destroy 1 card the opponent controls
-function s.costfilter(c,e,tp)
-	local attr=c:GetAttribute()
-	return c:IsType(TYPES) and c:IsAbleToRemoveAsCost() and s.attr_list[tp]&attr==0
-end
-function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil,e,tp)
-	e:SetLabel(g:GetFirst():GetAttribute())
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	local ct=e:GetLabel()
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetReset(RESET_EVENT+(RESETS_STANDARD&~RESET_TURN_SET))
+	e1:SetCountLimit(ct)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local att=e:GetLabel()
+	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
 	if #g>0 then
-		Duel.HintSelection(g,true)
-		Duel.Destroy(g,REASON_EFFECT)
-	end
-	s.attr_list[tp]=s.attr_list[tp]|att
-	for _,str in aux.GetAttributeStrings(att) do
-		e:GetHandler():RegisterFlagEffect(0,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,str)
+		Duel.HintSelection(g)
+		if Duel.Destroy(g:GetFirst(),REASON_EFFECT)~=0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			e1:SetValue(500)
+			c:RegisterEffect(e1)
+		end
 	end
 end
---(3.1)Register Attributes used
-function s.resetop(e,tp,eg,ep,ev,re,r,rp)
-	s.attr_list[0]=0
-	s.attr_list[1]=0
-	return false
+--(2)Destroy replace
+function s.repfilter(c,e)
+	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsMonster() and c:IsAbleToRemove()
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_GRAVE,0,1,c,e) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local g=Duel.SelectMatchingCard(tp,s.repfilter,tp,LOCATION_GRAVE,0,1,1,c,e)
+		e:SetLabelObject(g:GetFirst())
+		return true
+	else return false end
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+end
+--(3)Double damage
+function s.damcon(e)
+	return e:GetHandler():GetBattleTarget()~=nil
 end
