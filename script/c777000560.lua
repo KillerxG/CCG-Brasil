b5f4a59d14d25d1eb6d1cf850b2edd2ba08de4ee
KillerxG <guiros17@hotmail.com>
2025-05-26 08:51:47 -0300

    Changes
    • Arknights: Some art changes

--- script/c777000560.lua
+++ script/c777000560.lua
@@ -1,4 +1,4 @@
---Arknight - Reed
+--Arknights - Kal'tsit
 --Scripted by KillerxG
 local s,id=GetID()
 function s.initial_effect(c)
--- script/c777000610.lua
+++ script/c777000610.lua
@@ -1,4 +1,4 @@
---Arknight - Ifrit
+--Arknight - Reed
 --Scripted by KillerxG
 local s,id=GetID()
 function s.initial_effect(c)
