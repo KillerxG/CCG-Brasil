a86c12fa85af4222a939c7752a60ac56ea5a9c78
KillerxG <54422841+KillerxG@users.noreply.github.com>
2025-07-25 22:34:35 -0300

    Hack Cards
    Some RPG Player's hack cards.

---
+++ script/c747.lua
@@ -0,0 +1,151 @@
+--Azula Bellarosa
+--Scripted by KillerxG
+local s,id=GetID()
+function s.initial_effect(c)	
+	--(0)Banish at the Start of the Duel
+	local e0=Effect.CreateEffect(c)	
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_STARTUP)
+	e0:SetCountLimit(1)
+	e0:SetRange(LOCATION_HAND+LOCATION_DECK)
+	e0:SetOperation(s.op)
+	c:RegisterEffect(e0)
+	--(1)immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_ALL)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+	--(2)Restart Hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCode(EVENT_PREDRAW)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e2:SetCondition(s.rescon)
+	e2:SetTarget(s.restg)
+	e2:SetOperation(s.resop)
+	c:RegisterEffect(e2)
+	--(3)Destiny Draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PREDRAW)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1,id+1,EFFECT_COUNT_CODE_DUEL)
+	e3:SetCondition(s.condition)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+	--(4)Change 1 card from hand to other
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_REMOVED)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.drtg)
+	e4:SetOperation(s.drop)
+	c:RegisterEffect(e4)
+	--(5)Manipulate Fusion Summon for "Gem-Knight"
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,3))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_REMOVED)
+	e5:SetCountLimit(1,id)
+	e5:SetTarget(s.gemsp)
+	e5:SetOperation(s.gemop)
+	c:RegisterEffect(e5)
+end
+--(0)Activate
+function s.op(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+end
+--(1)immune
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+--(2)Restart Hand
+function s.rescon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==1
+end
+function s.restg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+end
+function s.resop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	if #g==0 then return end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	Duel.ShuffleDeck(tp)
+	Duel.BreakEffect()
+	Duel.Draw(tp,#g,REASON_EFFECT)
+end
+--(3)Destiny Draw
+function s.destinyfilter(c,e,tp)
+	return c:IsMonster() or c:IsSpellTrap()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and Duel.IsExistingMatchingCard(s.destinyfilter,tp,LOCATION_DECK,0,1,nil) and Duel.GetLP(tp)<=3000 and Duel.GetLP(tp)<Duel.GetLP(1-tp)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.SelectMatchingCard(tp,s.destinyfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.ShuffleDeck(tp)
+		Duel.MoveSequence(tc,0)
+		Duel.ConfirmDecktop(tp,1)
+	end
+end
+--(4)Change 1 card from hand to other
+function s.drfilter(c)
+	return c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeckAsCost,tp,LOCATION_HAND,0,1,nil) and Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end	
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetChainLimit(function(_e,_ep,_tp) return _tp==_ep end)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local f=Duel.SelectMatchingCard(tp,Card.IsAbleToDeckAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoDeck(f,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		--Duel.ConfirmCards(1-tp,g)
+	end
+end
+--(5)Manipulate Fusion Summon for "Gem-Knight"
+function s.gemfilter(c,e,tp)
+	return c:IsSetCard(SET_GEM_KNIGHT) and c:IsType(TYPE_FUSION) and c:GetLevel()<=8 and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,true) 
+end
+function s.gemsp(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.gemfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) and Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_HAND+LOCATION_MZONE,0,2,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetChainLimit(function(_e,_ep,_tp) return _tp==_ep end)
+end
+function s.gemop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,LOCATION_HAND+LOCATION_MZONE,0,2,2,nil)
+	Duel.SendtoGrave(tc,REASON_EFFECT)
+	local g=Duel.SelectMatchingCard(tp,s.gemfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local tf=g:GetFirst()
+	if not tc then return end
+	tf:SetMaterial(tc)
+	if Duel.SpecialSummon(tf,SUMMON_TYPE_FUSION,tp,tp,true,true,POS_FACEUP)~=0 then
+		
+	end
+end
