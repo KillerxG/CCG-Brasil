31cf52b22bf2124bae38826ff487718812660db9
KillerxG <rogsbxg@gmail.com>
2025-08-08 14:10:10 -0300

    News
    • Text Fix: Black Knight Xylia, Harmonic Xyz Fusion.
    • New Card: Harmonic Xyz Synchro.

--- script/c777005270.lua
+++ script/c777005270.lua
@@ -1,8 +1,8 @@
---Harmonic Xyz Fusion
+--Hamonic Xyz Synchro
 --Scripted by KillerxG
 local s,id=GetID()
-function s.initial_effect(c)
-	--(1)Special Summon Xyz and Fusion
+function s.initial_effect(c)	
+	--(1)Special Summon Xyz and Synchro
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -15,16 +15,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
---(1)Special Summon Xyz and Fusion
+--(1)Special Summon Xyz and Synchro
 function s.counterfilter(c)
-	return not (c:IsSummonLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION|TYPE_XYZ))
+	return not (c:IsSummonLocation(LOCATION_EXTRA) and not c:IsType(TYPE_XYZ|TYPE_SYNCHRO))
 end
 function s.cfilter1(c,e,tp)
-	return c:IsFaceup() and c:IsAbleToGraveAsCost() and not c:IsOriginalType(TYPE_TRAP)
+	return c:IsFaceup() and c:IsType(TYPE_TUNER) and c:IsAbleToGraveAsCost() and not c:IsOriginalType(TYPE_TRAP)
 		and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_MZONE,0,1,c,e,tp,c)
 end
 function s.cfilter2(c,e,tp,tun)
-	if not (c:IsFaceup() and c:IsAbleToGraveAsCost() and not c:IsOriginalType(TYPE_TRAP)) then return false end
+	if not (c:IsFaceup() and not c:IsType(TYPE_TUNER) and c:IsAbleToGraveAsCost() and not c:IsOriginalType(TYPE_TRAP)) then return false end
 	local g=Group.FromCards(tun,c)
 	for tc in g:Iter() do
 		tc:AssumeProperty(ASSUME_CODE,tc:GetOriginalCodeRule())
@@ -35,19 +35,19 @@ function s.cfilter2(c,e,tp,tun)
 		tc:AssumeProperty(ASSUME_ATTACK,tc:GetTextAttack())
 		tc:AssumeProperty(ASSUME_DEFENSE,tc:GetTextDefense())
 	end
-	local chk=Duel.GetLocationCountFromEx(tp,tp,g,TYPE_FUSION)>=2
+	local chk=Duel.GetLocationCountFromEx(tp,tp,g,TYPE_XYZ)>=2
 		and Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,g)
 		and Duel.IsExistingMatchingCard(s.syncfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,g)
 	Duel.AssumeReset()
 	return chk
 end
 function s.fusfilter(c,e,tp,mg)
-	if not (c:IsFacedown() and c:IsType(TYPE_FUSION) and (not c.material_location or (c.material_location&LOCATION_GRAVE)>0)) then return false end
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:CheckFusionMaterial(mg)
+	if not (c:IsFacedown() and c:IsType(TYPE_XYZ) and (not c.material_location or (c.material_location&LOCATION_GRAVE)>0)) then return false end
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsXyzSummonable(nil,mg)
 end
 function s.syncfilter(c,e,tp,mg)
-	if not (c:IsFacedown() and c:IsType(TYPE_XYZ)) then return false end
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsXyzSummonable(nil,mg)
+	if not (c:IsFacedown() and c:IsType(TYPE_SYNCHRO)) then return false end
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsSynchroSummonable(nil,mg)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
@@ -61,18 +61,18 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(g,REASON_COST)
 	Duel.SetTargetCard(g)
 	local c=e:GetHandler()
-	--Cannot Special Summon from the Extra Deck, except Fusion and Xyz Monsters
+	--Cannot Special Summon from the Extra Deck, except Xyz and Synchro Monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION|TYPE_XYZ) end)
+	e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_XYZ|TYPE_SYNCHRO) end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	--Clock Lizard check
-	aux.addTempLizardCheck(c,tp,function(_,c) return not c:IsOriginalType(TYPE_FUSION|TYPE_XYZ) end)
+	aux.addTempLizardCheck(c,tp,function(_,c) return not c:IsOriginalType(TYPE_XYZ|TYPE_SYNCHRO) end)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -84,7 +84,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetTargetCards(e)
-	if #mg~=2 or Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCountFromEx(tp,tp,mg,TYPE_FUSION)<2 then return end
+	if #mg~=2 or Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCountFromEx(tp,tp,mg,TYPE_XYZ)<2 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local fc=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,mg):GetFirst()
 	if not fc then return end
@@ -93,5 +93,5 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not sc then return end
 	local g=Group.FromCards(fc,sc)
 	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	Duel.Overlay(sc,mg)
+	Duel.Overlay(fc,mg)
 end
