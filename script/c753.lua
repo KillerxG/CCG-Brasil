f22a4e2286ffd355256cae9a9254714fac22fac8
KillerxG <54422841+KillerxG@users.noreply.github.com>
2025-09-23 19:51:18 -0300

    News
    • Bug Fix: Bestial Weapon Blade Fish text fix.
    • New Player Hack: Viktor Kyver

---
+++ script/c753.lua
@@ -0,0 +1,248 @@
+--Viktor Kyver
+local s,id=GetID()
+function s.initial_effect(c)	
+	--(0)Banish at the Start of the Duel
+	local e0=Effect.CreateEffect(c)	
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_STARTUP)
+	e0:SetCountLimit(1)
+	e0:SetRange(LOCATION_ALL)
+	e0:SetOperation(s.op)
+	c:RegisterEffect(e0)
+	--(1)immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_ALL)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+	--(2)Restart Hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCode(EVENT_PREDRAW)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e2:SetCondition(s.rescon)
+	e2:SetTarget(s.restg)
+	e2:SetOperation(s.resop)
+	c:RegisterEffect(e2)
+	--(3)Destiny Draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PREDRAW)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1,id+1,EFFECT_COUNT_CODE_DUEL)
+	e3:SetCondition(s.condition)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+	--(4)Change 1 card from hand to other
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_REMOVED)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.drtg)
+	e4:SetOperation(s.drop)
+	c:RegisterEffect(e4)
+	--(5)Cannot be Target
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_REMOVED)
+	e5:SetValue(s.efilter1)
+	c:RegisterEffect(e5)
+	--(6)Recover LP
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetCategory(CATEGORY_RECOVER)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetRange(LOCATION_REMOVED)
+	e6:SetOperation(s.lpop)
+	c:RegisterEffect(e6)
+	--(7)ATK Up
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,4))
+	e7:SetCategory(CATEGORY_ATKCHANGE)
+	e7:SetType(EFFECT_TYPE_IGNITION)
+	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e7:SetRange(LOCATION_REMOVED)
+	e7:SetTarget(s.atktg)
+	e7:SetOperation(s.atkop)
+	c:RegisterEffect(e7)
+	--Double Summon
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_IGNITION)
+	e8:SetDescription(aux.Stringid(id,6))
+	e8:SetRange(LOCATION_REMOVED)
+	e8:SetCountLimit(1)
+	e8:SetTarget(s.target)
+	e8:SetOperation(s.activate)
+	c:RegisterEffect(e8)
+		--Activate
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_IGNITION)
+	e9:SetDescription(aux.Stringid(id,5))
+	e9:SetRange(LOCATION_REMOVED)
+	e9:SetOperation(s.act)
+	c:RegisterEffect(e9)
+end
+--(0)Activate
+function s.op(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+end
+--(1)immune
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+--(2)Restart Hand
+function s.rescon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==1
+end
+function s.restg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+end
+function s.resop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	if #g==0 then return end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	Duel.ShuffleDeck(tp)
+	Duel.BreakEffect()
+	Duel.Draw(tp,#g,REASON_EFFECT)
+end
+--(3)Destiny Draw
+function s.destinyfilter(c,e,tp)
+	return c:IsMonster() or c:IsSpellTrap()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and Duel.IsExistingMatchingCard(s.destinyfilter,tp,LOCATION_DECK,0,1,nil) and Duel.GetLP(tp)<=3000 and Duel.GetLP(tp)<Duel.GetLP(1-tp)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.SelectMatchingCard(tp,s.destinyfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.ShuffleDeck(tp)
+		Duel.MoveSequence(tc,0)
+		Duel.ConfirmDecktop(tp,1)
+	end
+end
+--(4)Change 1 card from hand to other
+function s.drfilter(c)
+	return c:IsAbleToHand()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeckAsCost,tp,LOCATION_HAND,0,1,nil) and Duel.IsExistingMatchingCard(s.drfilter,tp,LOCATION_DECK,0,1,nil) end	
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetChainLimit(function(_e,_ep,_tp) return _tp==_ep end)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local f=Duel.SelectMatchingCard(tp,Card.IsAbleToDeckAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoDeck(f,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.drfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		--Duel.ConfirmCards(1-tp,g)
+	end
+end
+--(5)Cannot be Target
+function s.efilter1(e,re,rp)
+	return re:IsActiveType(TYPE_EFFECT)
+end
+--(6)Recover LP
+local declare_lp_table={}
+for i=1,30 do
+	declare_lp_table[i]=i*100
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	local ac=Duel.AnnounceNumber(tp,declare_lp_table)
+	Duel.Recover(tp,ac,REASON_EFFECT)
+end
+--(7)ATK Up
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local ac=Duel.AnnounceNumber(tp,declare_lp_table)
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESETS_STANDARD_PHASE_END,1)
+		e1:SetValue(ac)
+		tc:RegisterEffect(e1)
+	end
+end
+--double summon
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ct=0
+		local ce={Duel.IsPlayerAffectedByEffect(tp,EFFECT_SET_SUMMON_COUNT_LIMIT)}
+		for _,te in ipairs(ce) do
+			ct=math.max(ct,te:GetValue())
+		end
+		return ct<2
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_SUMMON_COUNT_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(2)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.act(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND,0):Filter(Card.IsLevelAbove,nil,1)
+	local tc=hg:GetFirst()
+	for tc in aux.Next(hg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(-9)
+		e1:SetReset(RESET_EVENT|(RESETS_STANDARD_PHASE_END&~RESET_TOFIELD))
+		tc:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_TO_HAND)
+	e2:SetReset(RESET_PHASE|PHASE_END)
+	e2:SetOperation(s.hlvop)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.hlvfilter(c,tp)
+	return c:IsLevelAbove(1) and c:IsControler(tp)
+end
+function s.hlvop(e,tp,eg,ep,ev,re,r,rp)
+	local hg=eg:Filter(s.hlvfilter,nil,tp)
+	local tc=hg:GetFirst()
+	for tc in aux.Next(hg) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(-9)
+		e1:SetReset(RESET_EVENT|(RESETS_STANDARD_PHASE_END&~RESET_TOFIELD))
+		tc:RegisterEffect(e1)
+	end
+end
