4afeb1ea291f242e4fb13afd7f4a6c85c81079c6
KillerxG <rogsbxg@gmail.com>
2026-06-05 10:25:49 -0300

    Update c161000000.lua

--- script/c161000000.lua
+++ script/c161000000.lua
@@ -32,6 +32,14 @@ if not ActionDuel then
 		ActionDuel.phase_used={[0]={},[1]={}}
 		ActionDuel.current_action_phase=0
 
+		local e1=Effect.GlobalEffect()
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_NO_TURN_RESET)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_ADJUST)
+		e1:SetCountLimit(1)
+		e1:SetOperation(ActionDuel.op)
+		Duel.RegisterEffect(e1,0)
+
 		local ep1=Effect.GlobalEffect()
 		ep1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ep1:SetCode(EVENT_PHASE+PHASE_MAIN1)
@@ -62,14 +70,7 @@ if not ActionDuel then
 		ep5:SetOperation(function() ActionDuel.current_action_phase=0 end)
 		Duel.RegisterEffect(ep5,0)
 
-		local e1=Effect.GlobalEffect()
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_NO_TURN_RESET)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_ADJUST)
-		e1:SetCountLimit(1)
-		e1:SetOperation(ActionDuel.op)
-		Duel.RegisterEffect(e1,0)
-
+		-- Add Action Card
 		local e2=Effect.GlobalEffect()
 		e2:SetDescription(aux.Stringid(id,0))
 		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -80,6 +81,7 @@ if not ActionDuel then
 		e2:SetCondition(ActionDuel.condition)
 		e2:SetTarget(ActionDuel.target)
 		e2:SetOperation(ActionDuel.operation)
+
 		local e3=e2:Clone()
 		e3:SetCode(EVENT_CHAINING)
 
@@ -94,6 +96,7 @@ if not ActionDuel then
 		e5:SetLabelObject(e3)
 		Duel.RegisterEffect(e5,0)
 
+		-- act ac in hand
 		local e6=Effect.GlobalEffect()
 		e6:SetType(EFFECT_TYPE_FIELD)
 		e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
@@ -112,6 +115,7 @@ if not ActionDuel then
 		e8:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
 		Duel.RegisterEffect(e8,0)
 
+		-- cover
 		local e9=Effect.GlobalEffect()
 		e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e9:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
@@ -120,6 +124,7 @@ if not ActionDuel then
 		e9:SetOperation(ActionDuel.coverop)
 		Duel.RegisterEffect(e9,0)
 
+		-- Link Summon restriction
 		local e10=Effect.GlobalEffect()
 		e10:SetType(EFFECT_TYPE_FIELD)
 		e10:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -128,6 +133,7 @@ if not ActionDuel then
 		e10:SetTarget(ActionDuel.linklimit)
 		Duel.RegisterEffect(e10,0)
 
+		-- Unlock Link Summons for the turn
 		local e11=Effect.GlobalEffect()
 		e11:SetDescription(aux.Stringid(id,10))
 		e11:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -145,6 +151,7 @@ if not ActionDuel then
 		e12:SetLabelObject(e11)
 		Duel.RegisterEffect(e12,0)
 
+		-- Field Spells are unaffected by effects
 		local e13=Effect.GlobalEffect()
 		e13:SetType(EFFECT_TYPE_SINGLE)
 		e13:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
@@ -237,8 +244,12 @@ if not ActionDuel then
 					table.insert(actionFieldToBeUsed,af)
 				end
 			end
-			if #actionFieldToBeUsed>0 then break
-			else Duel.Hint(HINT_MESSAGE,0,aux.Stringid(id,5)) Duel.Hint(HINT_MESSAGE,1,aux.Stringid(id,5)) end
+			if #actionFieldToBeUsed>0 then
+				break
+			else
+				Duel.Hint(HINT_MESSAGE,0,aux.Stringid(id,5))
+				Duel.Hint(HINT_MESSAGE,1,aux.Stringid(id,5))
+			end
 		end
 		if #actionFieldToBeUsed>1 then
 			Duel.Hint(HINT_MESSAGE,0,aux.Stringid(id,6))
@@ -362,40 +373,31 @@ if not ActionDuel then
 		return c and c.af and string.find(c.af,'m')
 	end
 
-	function ActionDuel.getpool(c)
-		return c and c.tableAction or tableActionGeneric
-	end
-
 	function ActionDuel.condition(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		local flag=ActionDuel.phaseflag()
-		local pool=ActionDuel.getpool(c)
 		return (not ActionDuel.handcheck(tp) or ActionDuel.fieldallowsmultiple(c))
 			and flag~=0
 			and not ActionDuel.phaseused(tp,flag)
 			and not c:IsStatus(STATUS_CHAINING)
-			and #pool>0
 	end
 
 	function ActionDuel.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		local c=e:GetHandler()
-		local pool=ActionDuel.getpool(c)
-		if chk==0 then return #pool>0 end
-		local ac=Duel.GetRandomNumber(1,#pool)
-		e:SetLabel(pool[ac])
+		local t=c.tableAction or tableActionGeneric
+		if chk==0 then return #t>0 end
+		local ac=Duel.GetRandomNumber(1,#t)
+		e:SetLabel(t[ac])
 	end
 
 	function ActionDuel.operation(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetCurrentChain()>0 and not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 
 		local c=e:GetHandler()
-		local flag=ActionDuel.phaseflag()
-		local pool=ActionDuel.getpool(c)
-
-		if flag==0 then return end
-		if #pool==0 then return end
 		if ActionDuel.handcheck(tp) and not ActionDuel.fieldallowsmultiple(c) then return end
-		if ActionDuel.phaseused(tp,flag) then return end
+
+		local flag=ActionDuel.phaseflag()
+		if flag==0 or ActionDuel.phaseused(tp,flag) then return end
 
 		ActionDuel.markphaseused(tp,flag)
 		Duel.RegisterFlagEffect(tp,flag,0,0,1)
@@ -419,99 +421,103 @@ if not ActionDuel then
 		if ActionDuel.fieldallowsmultiple(c) and c.tableAction then
 			ActionDuel.removepoolcard(c.tableAction,e:GetLabel())
 		end
+
 		if send_to_grave then
 			Duel.SendtoGrave(token,REASON_EFFECT)
 			return
 		end
+
 		Duel.SendtoHand(token,nil,REASON_EFFECT)
-		ActionDuel.chktrap(token,tokenp,e)
+		ActionDuel.chktrap(token,tokenp,e,eg,ep,ev,re,r,rp)
 	end
 
-	function ActionDuel.chktrap(tc,tp,e)
-		if tc and tc:IsTrap() and tc:CheckActivateEffect(false,false,false)
-			and Duel.GetLocationCount(tp,LOCATION_SZONE) then
-			Duel.ConfirmCards(1-tp,tc)
-			local tpe=tc:GetType()
-			local te=tc:GetActivateEffect()
-			local tg=te:GetTarget()
-			local co=te:GetCost()
-			local op=te:GetOperation()
-			e:SetCategory(te:GetCategory())
-			e:SetProperty(te:GetProperty())
-			Duel.ClearTargetCard()
-			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-			Duel.Hint(HINT_CARD,0,tc:GetCode())
-			tc:CreateEffectRelation(te)
-			if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
-			if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
-			Duel.BreakEffect()
-			local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-			if g then
-				for etc in aux.Next(g) do
-					etc:CreateEffectRelation(te)
-				end
+	function ActionDuel.chktrap(tc,tp,e,eg,ep,ev,re,r,rp)
+		if not (tc and tc:IsTrap()) then return end
+		if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+		if not tc:CheckActivateEffect(false,false,false) then return end
+
+		local te=tc:GetActivateEffect()
+		if not te then return end
+
+		Duel.ConfirmCards(1-tp,tc)
+		local tg=te:GetTarget()
+		local co=te:GetCost()
+		local op=te:GetOperation()
+
+		e:SetCategory(te:GetCategory())
+		e:SetProperty(te:GetProperty())
+		Duel.ClearTargetCard()
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		Duel.Hint(HINT_CARD,0,tc:GetCode())
+		tc:CreateEffectRelation(te)
+
+		if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
+		if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
+
+		Duel.BreakEffect()
+
+		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+		if g then
+			for etc in aux.Next(g) do
+				etc:CreateEffectRelation(te)
 			end
-			if op then op(te,tp,eg,ep,ev,re,r,rp) end
-			tc:ReleaseEffectRelation(te)
-			if g then
-				for etc in aux.Next(g) do
-					etc:ReleaseEffectRelation(te)
-				end
-			end
-			tc:SetStatus(STATUS_LEAVE_CONFIRMED,true)
-			tc:CancelToGrave(false)
-			Duel.SendtoGrave(tc,REASON_RULE)
 		end
+
+		if op then op(te,tp,eg,ep,ev,re,r,rp) end
+
+		tc:ReleaseEffectRelation(te)
+		if g then
+			for etc in aux.Next(g) do
+				etc:ReleaseEffectRelation(te)
+			end
+		end
+
+		tc:SetStatus(STATUS_LEAVE_CONFIRMED,true)
+		tc:CancelToGrave(false)
+		Duel.SendtoGrave(tc,REASON_RULE)
 	end
 
 	COVER_ACTION=301
 	CARD_VANILLA_MODE=511004400
 	CARD_POTENTIAL_YELL=511004399
 	CARD_ABILITY_YELL=511004401
-	CARD_ABILITY_YELL=511004401
 	CARD_EARTHBOUND_TUNDRA=150000000
 	ACTION_DUEL_LINK_UNLOCK=161999999
 
-	local tableActionGeneric={
-		150000024,150000033,
-		150000047,150000042,
-		150000011,150000044,
-		150000022,150000020
-	}
-
 	local OCGActionFields={
-	4064256,
-	59197169,
-	4545854,
-	2084239,
-	54306223,
-	23424603,
-	62188962,
-	82999629,
-	CARD_UMI,
-	86318356,
-	4215636,
-	32391631,
-	45778932,
-	94585852,
-	18161786,
-	50913601,
-	56074358,
-	80921533,
-	14001430,
-	CARD_UMI,
-	22751868,
-	75782277,
-	10080320,
-	7617062,
-	37694547,
-	33017655,
-	56594520,
-	87430998,
-	62265044,
-	78082039,
-	28120197,
-	85668449,
-	712559,
-	35956022}
+		4064256,
+		59197169,
+		4545854,
+		2084239,
+		54306223,
+		23424603,
+		62188962,
+		82999629,
+		CARD_UMI,
+		86318356,
+		4215636,
+		32391631,
+		45778932,
+		94585852,
+		18161786,
+		50913601,
+		56074358,
+		80921533,
+		14001430,
+		CARD_UMI,
+		22751868,
+		75782277,
+		10080320,
+		7617062,
+		37694547,
+		33017655,
+		56594520,
+		87430998,
+		62265044,
+		78082039,
+		28120197,
+		85668449,
+		712559,
+		35956022
+	}
 end
