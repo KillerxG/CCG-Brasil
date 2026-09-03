8f50ab5a13f9ba9f8a7f112150e652b82a96dfda
KillerxG <rogsbxg@gmail.com>
2026-09-03 09:26:00 -0300

    Testing v2

--- script/c776999880.lua
+++ script/c776999880.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTarget(s.atktg)
@@ -24,6 +24,29 @@ function s.initial_effect(c)
 	e2:SetCost(s.hrcost)
 	e2:SetOperation(s.hrop)
 	c:RegisterEffect(e2)
+	--Banish an opponent's card without targeting
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCost(s.rmcost)
+	e3:SetTarget(s.rmtg)
+	e3:SetOperation(s.rmop)
+	c:RegisterEffect(e3)
+	--Banish 2 monsters from the Extra Deck instead of destroying this card
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_DESTROY_REPLACE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.desreptg)
+	e4:SetOperation(s.desrepop)
+	c:RegisterEffect(e4)
 end
 
 function s.atkfilter(c,tp,hc)
@@ -53,8 +76,51 @@ end
 
 function s.hrcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsFaceup() and c:GetAttack()>=3000 end
-	c:UpdateAttack(-3000,RESET_EVENT|RESETS_STANDARD,c)
+	if chk==0 then return c:IsFaceup() and c:GetAttack()>=4000 end
+	c:UpdateAttack(-4000,RESET_EVENT|RESETS_STANDARD,c)
+end
+
+function s.exfilter(c)
+	return (c:IsRace(RACE_DRAGON) or c:IsAttribute(ATTRIBUTE_FIRE))
+		and c:IsAbleToRemoveAsCost()
+end
+
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.exfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+end
+
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local sg=g:Select(tp,1,1,nil)
+	Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+end
+
+function s.desrepfilter(c)
+	return (c:IsRace(RACE_DRAGON) or c:IsAttribute(ATTRIBUTE_FIRE)) and c:IsAbleToRemove()
+end
+
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReason(REASON_BATTLE|REASON_EFFECT)
+		and not c:IsReason(REASON_REPLACE)
+		and Duel.IsExistingMatchingCard(s.desrepfilter,tp,LOCATION_EXTRA,0,2,nil) end
+	return Duel.SelectEffectYesNo(tp,c,aux.Stringid(id,3))
+end
+
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+	local g=Duel.SelectMatchingCard(tp,s.desrepfilter,tp,LOCATION_EXTRA,0,2,2,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT|REASON_REPLACE)
 end
 
 function s.hrop(e,tp,eg,ep,ev,re,r,rp)
